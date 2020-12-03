1c8ce2010c1212b438a5694a51a1a487224d662d
Sally Chen <sally_chen@brown.edu>
2020-12-03 10:51:13 +0800

    added error msg

--- src/DisplayList.js
+++ src/DisplayList.js
@@ -6,35 +6,46 @@ import "./App.css";
 const DisplayList = (props) => {
   const { onAdd, list } = props;
 
+  console.log("list", list)
+
   return (
     <div className="card-wrapper">
-      {list.map((item) => (
-        <Card className="item-card" key={item.id}>
-          <Card.Img
-            variant="top"
-            className="product-image"
-            src={require("./assets/" + item.name + ".png").default}
-          />
-          <Card.Body>
-            <div className="product-content">
-              <Card.Title>{item.name}</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                {item.type}{" "}
-              </Card.Subtitle>
-              <Card.Subtitle className="mb-2 text-muted">
-                {item.region}{" "}
-              </Card.Subtitle>
-              <Card.Text>${item.price}</Card.Text>
-            </div>
+      {list && list.length > 0 ? (
+        list.map((item) => (
+          <Card className="item-card" key={item.id}>
+            <Card.Img
+              variant="top"
+              className="product-image"
+              src={require("./assets/" + item.name + ".png").default}
+            />
+            <Card.Body>
+              <div className="product-content">
+                <Card.Title>{item.name}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {item.type}{" "}
+                </Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {item.region}{" "}
+                </Card.Subtitle>
+                <Card.Text>${item.price}</Card.Text>
+              </div>
 
-            <div className="d-flex justify-content-center mt-2">
-              <Button variant="primary" size="sm" onClick={() => onAdd(item)} className="btn-block">
-                Add to cart
-              </Button>
-            </div>
-          </Card.Body>
-        </Card>
-      ))}
+              <div className="d-flex justify-content-center mt-2">
+                <Button
+                  variant="primary"
+                  size="sm"
+                  onClick={() => onAdd(item)}
+                  className="btn-block"
+                >
+                  Add to cart
+                </Button>
+              </div>
+            </Card.Body>
+          </Card>
+        ))
+      ) : (
+        <div className="px-2 h5">Uh-oh, nothing matching your filters :(</div>
+      )}
     </div>
   );
 };
